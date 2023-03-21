22cade2f240bd55a2f55c9ffbf20708e5adfd50b
Jared Nishikawa <jnishikawa@carbonblack.com>
2023-03-21 14:40:35 -0600

    dfs

--- satisfy.py
+++ satisfy.py
@@ -3,7 +3,8 @@
 class UnknownItemError(BaseException):
     pass
 
-raw = {"iron_ore", "coal"}
+
+raw = {"iron_ore", "coal", "copper_ore"}
 recipes = {
         "smart_plating": ({"reinforced_plate": 2, "rotor": 2}, 2),
         "reinforced_plate": ({"iron_plate": 30, "screw": 60}, 5),
@@ -16,47 +17,61 @@ recipes = {
         "modular_frame": ({"reinforced_plate": 3, "iron_rod": 12}, 2),
         "steel_beam": ({"steel_ingot": 60}, 15),
         "steel_ingot": ({"iron_ore": 45, "coal": 45}, 45),
+        "motor": ({"rotor": 10, "stator": 10}, 5),
+        "stator": ({"steel_pipe": 15, "wire": 40}, 5),
+        "steel_pipe": ({"steel_ingot": 30}, 20),
+        "wire": ({"copper_ingot": 15}, 30),
+        "copper_ingot": ({"copper_ore": 30}, 30),
         }
-        
 
-def add_cost(cost1, cost2):
-    c = {}
-    for item, value in cost1.items():
-        c[item] = value
-    for item, value in cost2.items():
-        if item not in c:
-            c[item] = 0
-        c[item] += value
-    return c
+class Node:
+    def __init__(self, name, edges, value):
+        self.name = name
+        self.edges = edges
+        self.value = value
+class Graph:
+    def __init__(self):
+        self.raw = raw
+        self.nodes = {}
+        for name, recipe in recipes.items():
+            n = Node(name, recipe[0], recipe[1])
+            self.nodes[name] = n
 
+    def visit(self, item, qty, verbose=False):
+        print(f"computing costs for {item} at {qty} units/min")
+        raw = {}
+        intermed = {}
+        stack = [(item, qty, 0)]
+        while stack:
+            name, q, depth = stack.pop()
+            if verbose:
+                print("    "*depth + f"{name}: {q}")
+            if name in self.raw:
+                if name not in raw:
+                    raw[name] = 0
+                raw[name] += q
+                continue
+            else:
+                if name not in intermed:
+                    intermed[name] = 0
+                intermed[name] += q
+            if name not in self.nodes:
+                raise UnknownItemError(name)
+            node = self.nodes[name]
+            rate = node.value
+            for ingredient, num in node.edges.items():
+                stack.append((ingredient, num*q/rate, depth+1))
+        print("total raw products")
+        print(raw)
+        print("total intermediate products")
+        print(intermed)
+        print("-"*30)
 
 # enter item and qty(/min) needed
-def compute_cost(item, qty):
-    cost = {}
-    if item not in recipes:
-        raise UnknownItemError(item)
-    recipe = recipes[item]
-    rate = recipe[1]
-    for ingredient, num in recipe[0].items():
-        if ingredient not in cost:
-            cost[ingredient] = 0
-        cost[ingredient] += num * (qty/rate)
-    return cost
-
-
-def to_raw(cost):
-    raw_cost = {}
-    for ingredient, num in cost.items():
-        if ingredient not in raw:
-            c = compute_cost(ingredient, num)
-            r = to_raw(c)
-            raw_cost = add_cost(raw_cost, r)
-        else:
-            raw_cost = add_cost(raw_cost, {ingredient: num})
-    return raw_cost
-
 if __name__ == '__main__':
-    c = compute_cost("smart_plating", 16)
-    print(to_raw(c))
-    print(to_raw(compute_cost("versatile_framework", 10)))
+    # initialize graph
+    g = Graph()
+    g.visit("versatile_framework", 16, verbose=True)
+    g.visit("smart_plating", 2, verbose=True)
+    g.visit("motor", 5, verbose=True)
 
